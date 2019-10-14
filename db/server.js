d064eaaa6bf9d0b6f0cf430ada9bc460f3f42901
luo-mini <1009183116@qq.com>
2019-10-14 21:39:47 +0800

    server

--- db/server.js
+++ db/server.js
@@ -24,13 +24,8 @@ http.createServer((req, res) => {
       str += data
     })
   }
-<<<<<<< HEAD:build/dev-server.js
-
-  //req.on('end', function (err) {
-=======
   req.on('end', () => {
     console.log(url.pathname.slice(4))
->>>>>>> b3ba6d569157752f910414ae95acf50ea27a0060:db/server.js
     getApi(url.pathname.slice(4), querystring.parse(str)).then(result => {
       if (url.pathname.slice(4) === '/post/login') {
         tokens.push(result.token)
@@ -45,26 +40,10 @@ http.createServer((req, res) => {
         res.end(JSON.stringify({code: 400, msg: err}))
       }
     })
-  //})
+  })
 }).listen(3000, () => {
   console.log('网络连接已打开：http://127.0.0.1:3000')
 })
-
-// var users = {};
-
-// var server = http.createServer(function(rarse(str);
-//     console.log(url,GET,POST);
-//     //如果请求的是user就认定为    break;
-//         case 'login':
-//           //检查用户是否存在
-//           if(users[GET.user] == null){
-//             res.write('{"ok":false,"msg":"该用户不存在"}');
-//           }else if(users[GET.user] !== GET.pass){
-//             //检查密码是否正确
-//             res.write('{"ok":false,"msg":"账户或密码错误"}');
-//           }else{
-//             res.write('{"ok":true,"msg":"登陆成功"}');
-//           }
 
 const app = new Koa()
 let ws_server = app.listen(9001)
